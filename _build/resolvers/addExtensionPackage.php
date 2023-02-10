5292eb870f39379f2ef1a843acee6356fb454d69
Semyon Zhukov <88970712+SZhukovWork@users.noreply.github.com>
2023-02-11 00:24:06 +0500

    Update addExtensionPackage.php

--- _build/resolvers/addExtensionPackage.php
+++ _build/resolvers/addExtensionPackage.php
@@ -6,10 +6,15 @@ if ($transport->xpdo) {
     $modx =& $transport->xpdo;
 
     switch ($options[xPDOTransport::PACKAGE_ACTION]) {
-        case xPDOTransport::ACTION_INSTALL:
-            $modx->addExtensionPackage($transport->name, "[[++core_path]]components/{$transport->name}/model/");
-            break;
         case xPDOTransport::ACTION_UPGRADE:
+            $modx->removeExtensionPackage($transport->name);
+        case xPDOTransport::ACTION_INSTALL:
+            $ns = $modx->getObject("modNamespace",$transport->name);
+            if(empty($ns)){
+                $modx->log(MODX_LOG_LEVEL_ERROR,"Namespace not found");
+                break;
+            }
+            $modx->addExtensionPackage($transport->name, str_replace("{core_path}","[[++core_path]]",$ns->get("path"))."model/");
             break;
         case xPDOTransport::ACTION_UNINSTALL:
             break;
