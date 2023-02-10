bd89c6c7f65695a820fee4ec6653cd8fe3883a25
Semyon Zhukov <88970712+SZhukovWork@users.noreply.github.com>
2023-02-10 23:06:02 +0500

    Update _policies.php

--- _build/elements/_policies.php
+++ _build/elements/_policies.php
@@ -1,10 +1,11 @@
 <?php
 
 return [
+    /* example
     'modExtraUserPolicy' => [
         'description' => 'modExtra policy description.',
         'data' => [
             'modextra_save' => true,
         ]
-    ],
+    ],*/
 ];
